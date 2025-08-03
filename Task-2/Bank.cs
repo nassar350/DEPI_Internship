41879763ef13be18c5f1ee5646ec5f57264cfe06
Mohamed Nassar <126024444+nassar350@users.noreply.github.com>
2025-08-03 22:08:25 +0300

    Bank System

---
+++ Task-2/Bank.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    public class Bank
+    {
+        const string Bank_code = "BNK001";
+        readonly DateTime Created_date;
+        readonly int AccountNumber;
+
+        private int _Phonenumber;
+        private string _NationalID;
+        private string _Fullname;
+        private string _Address;
+        private int _Balance;
+
+        public string Fullname { 
+            get
+            { 
+                return _Fullname;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    Console.WriteLine("Invalid Name");
+                }
+                else
+                {
+                    _Fullname = value;
+                }
+            }
+        }
+
+        public string NationalID
+        {
+            get
+            {
+                return _NationalID;
+            }
+            set
+            {
+                if (value.Length == 14)
+                {
+                    Console.WriteLine("Invalid National ID");
+                }
+                else
+                {
+                    _NationalID = value;
+                }
+            }
+        }
+
+        public int Phonenumber
+        {
+            get
+            {
+                return _Phonenumber;
+            }
+            set
+            {
+                string number = Convert.ToString(value);
+                if (number.Length == 11 && number[0] == '0' && number[1] == '1')
+                {
+                    _Phonenumber = value;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Phone Number");
+                }
+            }
+        }
+
+        public int Balance
+        {
+            get
+            {
+                return _Balance;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    _Balance = value;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Balance Must Be Positive");
+                }
+            }
+        }
+
+        public string Address { 
+            get
+            {
+                return _Address;
+            }
+            set 
+            {
+                if (value.Length == 0)
+                {
+                    Console.WriteLine("Enter your Address");
+                }
+                else
+                {
+                    _Address = value;
+                }
+            }
+        }
+
+
+        public Bank()
+        {
+            _Balance = 0;
+            _Fullname = "user";
+            _NationalID = "no ID";
+            _Phonenumber = 0;
+            _Address = "no address";
+            AccountNumber = 0;
+            Created_date = DateTime.Now;
+        }
+        public Bank(string full_name, string nationalID, int phonenumber, string address, int accountnumber)
+        {
+            _Fullname = full_name;
+            _NationalID = nationalID;
+            _Phonenumber = phonenumber;
+            _Address = address;
+            _Balance = 0;
+            accountnumber = accountnumber;
+            Created_date = DateTime.Now;
+        }
+
+        public Bank(string full_name, string nationalID, int phonenumber, string address, int accountnumber, int balance):this (full_name,nationalID,phonenumber,address, accountnumber)
+        {
+            _Balance = balance;
+
+            Created_date = DateTime.Now;
+        }
+
+
+        public void showAccountDetails()
+        {
+            Console.WriteLine($"name: {Fullname}, phone: {Phonenumber}, balance: {Balance}, address: {Address}, national ID: {NationalID}");
+        }
+
+        bool isValidNationalID()
+        {
+            if (_NationalID.Length == 14)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        bool isValidPhoneNumber()
+        {
+            string pnumber = Convert.ToString(_Phonenumber);
+            if (pnumber.Length == 11 && pnumber[0] == '0' && pnumber[1] == '1')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Task-2/Program.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank bank1 = new Bank();
+            Bank bank2 = new Bank("Ahmed", "14578123471587", 01274828887, "cairo", 4012, 1500);
+            Bank bank3 = new Bank("Ali", "47812479324150", 01074820087, "Nasr city", 8574);
+
+            bank1.showAccountDetails();
+            Console.WriteLine("===============================");
+            bank2.showAccountDetails();
+            Console.WriteLine("===============================");
+            bank3.showAccountDetails();
+            Console.WriteLine("===============================");
+        }
+    }
+}
