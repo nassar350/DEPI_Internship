3a359f70f03a4b8495936af7b109e6827d80302e
Mohamed Nassar <126024444+nassar350@users.noreply.github.com>
2025-09-21 23:32:15 +0300

    Initialize

---
+++ EmployeeAPI/Data/EmployeeAppContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace EmployeeAPI.Data
+{
+    public class EmployeeAppContext : DbContext
+    {
+        public EmployeeAppContext(DbContextOptions<EmployeeAppContext>options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ EmployeeAPI/Services/EmployeeService.cs
@@ -0,0 +1,50 @@
+﻿using EmployeeAPI.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAPI.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        public readonly EmployeeAppContext _context;
+        public EmployeeService(EmployeeAppContext context) 
+        {
+            _context = context;
+        }
+        public async Task<ActionResult<Employee>> AddEmployee(Employee employee)
+        {
+            _context.Employees.Add(employee);
+            _context.SaveChanges();
+            return employee;
+        }
+
+        public async Task<ActionResult<bool>> DeleteEmployee(int id)
+        {
+            Employee employee = _context.Employees.Find(id);
+            _context.Employees.Remove(employee);
+            _context.SaveChanges();
+            return true;
+        }
+
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployee()
+        {
+            await _context.SaveChangesAsync();
+            return _context.Employees.ToArray();
+        }
+
+        public async Task<ActionResult<Employee>> GetEmployeeById(int id)
+        {
+            Employee employee = _context.Employees.Find(id);
+            return employee;
+        }
+
+        public async Task<ActionResult<Employee>> UpdateEmployee(Employee employee, int id)
+        {
+            Employee emp = _context.Employees.Find(id);
+            emp.Name = employee.Name;
+            emp.Email = employee.Email;
+            _context.Employees.Update(emp);
+            _context.SaveChanges();
+            return emp;
+        }
+    }
+}
